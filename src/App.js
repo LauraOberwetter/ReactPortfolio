68f8fd6bdaabd4408463dad7c7325479da624860
Laura Oberwetter <leoberwetter@gmail.com>
2021-07-05 17:17:20 -0500

    header created

--- src/App.js
+++ src/App.js
@@ -1,58 +1,63 @@
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Container from 'react-bootstrap/Container';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 
-import Footer from './Components/Footer';
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 class App extends React.Component {
-
-  
-  constructor(props) {  //constructor gets props
+  constructor(props) {
+    //constructor gets props
     super(props); //super takes in props
-    this.state = { //establish state for top level website items
-        title: 'Laura Oberwetter',
-        headerLinks: [ //array of objects with navbar titles/links
-          {title: 'Home', path: '/'},
-          {title: 'About', path: '/about'},
-          {title: 'Contact', path: '/contact'},
-        ],
-        //top level information for each specific page
-        home: { // home object
-          title: 'Laura Oberwetter',
-          subTitle: 'Portfolio',
-          text: 'whatever blah blah'
-        },
-        about: { // about object
-          title: 'About Laura',
-        },
-        contact: { // contact object
-          title: 'HMU',
-        }
-    }
+    this.state = {
+      //establish state for top level website items
+      title: "Laura Oberwetter",
+      headerLinks: [
+        //array of objects with navbar titles/links
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" },
+      ],
+      //top level information for each specific page
+      home: {
+        // home object
+        title: "Laura Oberwetter",
+        subTitle: "Portfolio",
+        text: "whatever blah blah",
+      },
+      about: {
+        // about object
+        title: "About Laura",
+      },
+      contact: {
+        // contact object
+        title: "HMU",
+      },
+    };
   }
-  render () {
+  render() {
     return (
       <Router>
         <Container flud={true}>
+          <Header />
           {/* main nav section */}
           <nav class="container h-100">
             <div class="row menu">
               <div class="col-sm d-flex align-items-center justify-content-center red">
                 <button class="animation scale-in pinkBtn">
-                  <a href="#About" class="blackLink">About</a>
+                  <a href="#About" class="blackLink">
+                    About
+                  </a>
                   {/* or */}
                   {/* <Link className="blackLink" to="#About"></Link> */}
                 </button>
               </div>
               <div class="col-sm d-flex align-items-center justify-content-center pink">
                 <button class="animation scale-in redBtn">
-                  <a href="#Work" class="whiteLink">Work</a>
+                  <a href="#Work" class="whiteLink">
+                    Work
+                  </a>
                   {/* or */}
                   {/* <Link className="whiteLink" to="#Work"></Link> */}
                 </button>
@@ -61,15 +66,15 @@ class App extends React.Component {
                 <button class="animation scale-in pinkBtn">
                   {/* <a href="#Contact" class="blackLink">Contact</a> */}
                   {/* or */}
-                  <Link className="blackLink" to="#Contact">Contact</Link>
+                  <Link className="blackLink" to="#Contact">
+                    Contact
+                  </Link>
                 </button>
               </div>
             </div>
           </nav>
 
-          {/* footer section */}
           <Footer />
-
         </Container>
         <Switch>
           <Route></Route>
@@ -77,8 +82,6 @@ class App extends React.Component {
       </Router>
     );
   }
-  
 }
-
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+
+function Header() {
+    return(
+        // adjust the margin top to be different size probably
+        <header className="mb-5"> 
+            <Container fluid={true}>
+                <Row className='headBanner border-bottom text-center vhs-flicker vhs-delay-3 vhs-duration-6 js-demo'>
+                    Laura Oberwetter
+                </Row>
+            </Container>
+
+        </header>
+    )
+
+}
+
+export default Header;
+
